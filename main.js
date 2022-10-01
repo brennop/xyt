7beadcca673b294dcc683710c7cdad729185dabc
brennop <brennopcordeiro@gmail.com>
2022-09-30 22:27:45 -0300

    more operators

--- main.js
+++ main.js
@@ -6,22 +6,22 @@ const debug = document.querySelector(".debug");
 
 // define a 16 colors palette
 const pallet = {
-  0: "#000000",
-  1: "#005500",
-  2: "#00aa00",
-  3: "#00ff00",
-  4: "#0000ff",
-  5: "#0055ff",
-  6: "#00aaff",
-  7: "#00ffff",
-  8: "#ff0000",
-  9: "#ff5500",
-  10: "#ffaa00",
-  11: "#ffff00",
-  12: "#ff00ff",
-  13: "#ff55ff",
-  14: "#ffaaff",
-  15: "#ffffff",
+  0: "#fbffce",
+  1: "#b4dc25",
+  2: "#26a630",
+  3: "#5af0f7",
+  4: "#fbd439",
+  5: "#ff9cc9",
+  6: "#25e2c0",
+  7: "#08a0c0",
+  8: "#f09432",
+  9: "#f43666",
+  10: "#c635bc",
+  11: "#165a7d",
+  12: "#dc532d",
+  13: "#a12536",
+  14: "#6f288b",
+  15: "#260e3e",
 };
 
 const SIZE = 16;
@@ -34,38 +34,46 @@ const data = {
 };
 
 const lookup = {
-  x: (x, y, t, i, s) => [...s, x * RATIO],
-  y: (x, y, t, i, s) => [...s, y * RATIO],
-  t: (x, y, t, i, s) => [...s, t],
-  i: (x, y, t, i, s) => [...s, i],
-  "+": (x, y, t, i, [a, b, ...s]) => [...s, a + b],
-  "-": (x, y, t, i, [a, b, ...s]) => [...s, a - b],
-  "*": (x, y, t, i, [a, b, ...s]) => [...s, a * b],
-  "/": (x, y, t, i, [a, b, ...s]) => [...s, a / b],
-  "%": (x, y, t, i, [a, b, ...s]) => [...s, a % b],
-  "&": (x, y, t, i, [a, b, ...s]) => [...s, a & b],
-  "|": (x, y, t, i, [a, b, ...s]) => [...s, a | b],
-  "^": (x, y, t, i, [a, b, ...s]) => [...s, a ^ b],
-  S: (x, y, t, i, [a, ...s]) => [...s, Math.sin(a)],
-  C: (x, y, t, i, [a, ...s]) => [...s, Math.cos(a)],
-  T: (x, y, t, i, [a, ...s]) => [...s, Math.tan(a)],
+  x: (x, y, t, i, s) => [x * RATIO, ...s],
+  y: (x, y, t, i, s) => [y * RATIO, ...s],
+  t: (x, y, t, i, s) => [t / 256, ...s],
+  i: (x, y, t, i, s) => [i, ...s],
+  "+": (x, y, t, i, [a, b, ...s]) => [a + b, ...s],
+  "-": (x, y, t, i, [a, b, ...s]) => [a - b, ...s],
+  "*": (x, y, t, i, [a, b, ...s]) => [a * b, ...s],
+  "/": (x, y, t, i, [a, b, ...s]) => [a / b, ...s],
+  "%": (x, y, t, i, [a, b, ...s]) => [a % b, ...s],
+  "&": (x, y, t, i, [a, b, ...s]) => [a & b, ...s],
+  "|": (x, y, t, i, [a, b, ...s]) => [a | b, ...s],
+  "^": (x, y, t, i, [a, b, ...s]) => [a ^ b, ...s],
+  S: (x, y, t, i, [a, ...s]) => [Math.sin(a), ...s],
+  C: (x, y, t, i, [a, ...s]) => [Math.cos(a), ...s],
+  T: (x, y, t, i, [a, ...s]) => [Math.tan(a), ...s],
+  V: (x, y, t, i, [a, ...s]) => [Math.sqrt(a), ...s],
+  R: (x, y, t, i, s) => [Math.random() * 16, ...s],
+  D: (x, y, t, i, [a, ...s]) => [a, a, ...s],
+  N: (x, y, t, i, [a, ...s]) => [(a + 1) * 8, ...s],
+
+
   " ": (x, y, t, i, s) => s,
-  0: (x, y, t, i, s) => [...s, 0],
-  1: (x, y, t, i, s) => [...s, 1],
-  2: (x, y, t, i, s) => [...s, 2],
-  3: (x, y, t, i, s) => [...s, 3],
-  4: (x, y, t, i, s) => [...s, 4],
-  5: (x, y, t, i, s) => [...s, 5],
-  6: (x, y, t, i, s) => [...s, 6],
-  7: (x, y, t, i, s) => [...s, 7],
-  8: (x, y, t, i, s) => [...s, 8],
-  9: (x, y, t, i, s) => [...s, 9],
-  a: (x, y, t, i, s) => [...s, 10],
-  b: (x, y, t, i, s) => [...s, 11],
-  c: (x, y, t, i, s) => [...s, 12],
-  d: (x, y, t, i, s) => [...s, 13],
-  e: (x, y, t, i, s) => [...s, 14],
-  f: (x, y, t, i, s) => [...s, 15],
+  "(": (x, y, t, i, s) => s,
+  ")": (x, y, t, i, s) => s,
+  0: (x, y, t, i, s) => [0, ...s],
+  1: (x, y, t, i, s) => [1, ...s],
+  2: (x, y, t, i, s) => [2, ...s],
+  3: (x, y, t, i, s) => [3, ...s],
+  4: (x, y, t, i, s) => [4, ...s],
+  5: (x, y, t, i, s) => [5, ...s],
+  6: (x, y, t, i, s) => [6, ...s],
+  7: (x, y, t, i, s) => [7, ...s],
+  8: (x, y, t, i, s) => [8, ...s],
+  9: (x, y, t, i, s) => [9, ...s],
+  a: (x, y, t, i, s) => [10, ...s],
+  b: (x, y, t, i, s) => [11, ...s],
+  c: (x, y, t, i, s) => [12, ...s],
+  d: (x, y, t, i, s) => [13, ...s],
+  e: (x, y, t, i, s) => [14, ...s],
+  f: (x, y, t, i, s) => [15, ...s],
 };
 
 function _eval(expr, x, y, t, i) {
@@ -77,7 +85,7 @@ function _eval(expr, x, y, t, i) {
 function render(t) {
   for (let i = 0; i < SIZE; i++) {
     for (let j = 0; j < SIZE; j++) {
-      const value = _eval(data.expr, i, j, t)
+      const [value] = _eval(data.expr, i, j, t, i * SIZE + j);
       const color = pallet[Math.floor(value) & 0xf];
       context.fillStyle = color;
       context.fillRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
