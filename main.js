0b72282eb15d79321f3c743d056679c23b6c1a84
brennop <brennopcordeiro@gmail.com>
2023-02-26 14:01:05 -0300

    with qr

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ import "./style.css";
 import "aframe"
 import "@ar-js-org/ar.js/aframe/build/aframe-ar";
 
-import { draw } from "./draw"; 
+import { draw } from "./draw";
+import jsQR from "jsqr";
 
 AFRAME.registerComponent('canvas-updater', {
   dependencies: ['geometry', 'material'],
 
-  tick: function () {
+  tick: function() {
     var el = this.el;
     var material;
 
@@ -21,15 +22,16 @@ AFRAME.registerComponent('canvas-updater', {
 const main = document.querySelector("body");
 main.innerHTML = `
         <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;'>
-          <a-assets position="0 0.5 0">
+          <a-assets position="0 0 0">
             <canvas id="canvas" width="256" height="256"></canvas>
           </a-assets>
 
-          <a-marker type='barcode' value='16'>
+          <a-marker type='pattern' url='pattern.patt'>
             <a-entity geometry='primitive: plane'
                       material='src: #canvas'
                       position='0 0 0'
                       rotation='-90 0 0'
+                      scale='3 3 3'
                       canvas-updater>
             </a-entity>
           </a-marker>
@@ -40,11 +42,52 @@ main.innerHTML = `
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-const expr = "xy+t+";
+const url = new URL(window.location);
+let expr = url.pathname.slice(1, 24);
+if (!expr) {
+  window.history.pushState({}, "", "/xy+");
+  expr = "xy+";
+}
+
+let video = null;
+const videoCanvas = document.createElement("canvas");
 
 function render(t) {
   draw(ctx, expr, t);
+
+  if (video) {
+    try {
+      // get the video frame
+      videoCanvas.width = video.videoWidth;
+      videoCanvas.height = video.videoHeight;
+      videoCanvas.getContext("2d").drawImage(video, 0, 0);
+      const imageData = videoCanvas.getContext("2d").getImageData(0, 0, video.videoWidth, video.videoHeight);
+      const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+      if (code) {
+        expr = code.data;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   requestAnimationFrame(render);
 }
 
 requestAnimationFrame(render);
+
+// poll for video
+function pollVideo() {
+  if (video) {
+    return;
+  }
+  video = document.querySelector("video");
+  if (video) {
+    console.log("video found");
+  } else {
+    setTimeout(pollVideo, 100);
+  }
+}
+
+pollVideo();
